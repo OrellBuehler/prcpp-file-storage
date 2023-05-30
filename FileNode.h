9a7235c29a296169eb485b918e2d24ded9cb36fd
Orell Bühler <orell.buehler@students.fhnw.ch>
2023-05-30 12:44:45 +0200

    feat: update FileNode

--- FileNode.h
+++ FileNode.h
@@ -7,6 +7,7 @@
 #include <fstream>
 #include <map>
 #include <variant>
+#include <map>
 
 class FileStorage;
 
@@ -51,5 +52,71 @@ private:
         assert(m_data.index() == 1);
     }
 
-    void parse() {}
+    std::map<Key, FileNode>& map() {
+        return std::get<std::map<Key, FileNode>>(m_data);
+    }
+
+    const std::map<Key, FileNode>& map() const {
+        return std::get<std::map<Key, FileNode>>(m_data);
+    }
+
+    const std::string& text() const {
+        return std::get<std::string>(m_data);
+    }
+
+    void parse() {
+        char delimiter;
+
+        m_is >> std::ws; // skip whitespaces
+        m_is >> delimiter; // read '{'
+
+        while(delimiter != '}') {
+            Key key;
+
+            m_is >> key;
+            key = key.substr(0, key.size() - 1); // remove ':'
+
+            m_is >> std::ws; // skip whitespaces and check next character
+            switch (m_is.peek())
+            {
+                case '{':
+                {
+                    // start a new object
+                    auto [it, inserted] = map().emplace(key, FileNode(m_is));
+                    if (!inserted) throw std::runtime_error("key " + key + " already exists");
+                    break;
+                }
+
+                case '"':
+                {
+                    // read a string
+                    std::string t;
+                    m_is.get(); // read '"'
+                    std::getline(m_is, t, '"');
+                    auto [it, inserted] = map().emplace(key, FileNode(*this, t));
+                    if (!inserted) throw std::runtime_error("key " + key + " already exists");
+                    break;
+                }
+
+                default:
+                {
+                    // read value
+                    std::string t;
+                    m_is >> t;
+
+                    if(t.back() == ',') {
+                        t = t.substr(0, key.size() - 1);
+                        m_is.unget(); // put ',' back to stream
+                    }
+
+                    auto [it, inserted] = map().emplace(key, FileNode(*this, t));
+                    if (!inserted) throw std::runtime_error("key " + key + " already exists");
+
+                    m_is >> std::ws;
+                    m_is >> delimiter;
+                    break;
+                }
+            }
+        }
+    }
 };
